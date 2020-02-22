6e95a46ff28a2574d76baeafc3191ea567073f1b
pe5 <eero.perola@helsinki.fi>
2020-02-22 22:09:57 +0200

    teht10x2

---
+++ bloglist-frontend/src/components/BlogForm.js
@@ -0,0 +1,58 @@
+import React, {useState} from 'react'
+import PropTypes from 'prop-types'
+
+const BlogForm = ({ createBlog }) => {
+    const [newTitle, setNewTitle] = useState('')
+    const [newAuthor, setNewAuthor] = useState('')
+    const [newUrl, setNewUrl] = useState('')
+
+    const handleTitleChange = (event) => {
+        setNewTitle(event.target.value)
+    }
+
+    const handleAuthorChange = (event) => {
+        setNewAuthor(event.target.value)
+    }
+
+    const handleUrlChange = (event) => {
+        setNewUrl(event.target.value)
+    }
+    
+    const addBlog = (event) => {
+        event.preventDefault()
+        createBlog({
+            title: newTitle,
+            author: newAuthor,
+            url: newUrl,
+            likes: 0
+        })
+        setNewTitle('')
+        setNewAuthor('')
+        setNewUrl('')
+    }
+
+    return (
+    <div>
+      <form onSubmit={addBlog}>
+      title: <input
+        value={newTitle}
+        onChange={handleTitleChange}
+      /><br />
+      author: <input
+        value={newAuthor}
+        onChange={handleAuthorChange}
+      /><br />
+      url: <input
+        value={newUrl}
+        onChange={handleUrlChange}
+      /><br />
+      <button type="submit">save</button>
+    </form> 
+      </div>
+  )}
+
+  BlogForm.propTypes = {
+      createBlog: PropTypes.func.isRequired
+  }
+
+export default BlogForm
